53be86f73f958db52cb87a296eae6317db6142b4
Rizky Novando Priyadi <rizkynovando@gmail.com>
2024-09-10 13:19:34 +0700

    fix: strip function not mutating]

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -6,7 +6,7 @@ export const stripDash = (uuid: string) => {
 export const stripDashAll = (uuids: string[]) => {
   const newUuids = []
   for (let uuid of uuids) {
-    if (uuid.length === 36 && uuid.includes('-')) uuid.replaceAll('-', '').toLowerCase()
+    if (uuid.length === 36 && uuid.includes('-')) uuid = uuid.replaceAll('-', '').toLowerCase()
     newUuids.push(uuid)
   }
   return newUuids
